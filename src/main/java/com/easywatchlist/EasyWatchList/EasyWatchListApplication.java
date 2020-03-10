942fca198c2b6b45d7f1358a4952805e607bd325
Attila Murvai <at.murvai@gmail.com>
2020-03-10 23:25:11 +0100

    Add spring template

---
+++ src/main/java/com/easywatchlist/EasyWatchList/EasyWatchListApplication.java
@@ -0,0 +1,13 @@
+package com.easywatchlist.EasyWatchList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyWatchListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyWatchListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easywatchlist/EasyWatchList/EasyWatchListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easywatchlist.EasyWatchList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyWatchListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
